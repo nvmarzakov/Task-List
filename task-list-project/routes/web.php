d17b9cd9d5d759a040a2c034d62582462fff25cc
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2024-03-05 11:47:57 +0200

    Forms and CSRF Protection,Validating and Storing Data

--- task-list-project/routes/web.php
+++ task-list-project/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,22 +22,36 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => \App\Models\Task::latest()->get()
+        'tasks' => Task::latest()->get()
     ]);
 })->name('tasks.index');
+
+Route::view('/tasks/create', 'create')
+    ->name('tasks.create');
 
 Route::get('/tasks/{id}/', function ($id) {
 
     return view('show', [
-        'task' => \App\Models\Task::findOrFail($id)
+        'task' => Task::findOrFail($id)
     ]);
 })->name('tasks.show');
 
+Route::post('/tasks', function (Request $request) {
+   $data = $request->validate([
+      'title' => 'required|max:255',
+      'description' => 'required',
+      'long_description' => 'required'
+   ]);
 
-//Route::get('/tasks/{id}', function ($id) {
-//
-//    return view('show', ['task'=>\App\Models\Task::find($id)]);
-//})->name('tasks.show');
+   $task = new Task;
+   $task->title = $data['title'];
+   $task->description = $data['description'];
+   $task->long_description = $data['long_description'];
+   $task->save();
+
+   return redirect()->route('tasks.show', ['id' => $task->id]);
+
+})->name('tasks.store');
 
 Route::fallback(function () {
     return 'Still got somewhere!';
