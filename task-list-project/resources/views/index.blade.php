18d39e44c39bcef13188ce897476cbd1c3ecce6b
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2024-03-13 23:22:22 +0200

    Change styling with Bootstrap
    - refactor index page

--- task-list-project/resources/views/index.blade.php
+++ task-list-project/resources/views/index.blade.php
@@ -4,24 +4,43 @@
 
 
 @section('content')
-    <nav class="mb-4">
-        <a href="{{ route('tasks.create') }}"
-        class="font-medium text-700 underline decoration-pink-500">Add Task</a>
-    </nav>
-    @forelse($tasks as $task)
-        <div>
-            <a href="{{ route('tasks.show', ['task' => $task->id]) }}"
-            @class(['line-through' => $task->completed])> {{ $task->title }}</a>
+    <section class="card shadow-lg bg-primary-subtle text-secondary-emphasis">
+        <div class="row">
+            <div class="col-12 d-flex align-items-center justify-content-center">
+                <nav class="my-4 text-center">
+                    <a href="{{ route('tasks.create') }}"
+                       class="btn btn-dark text-decoration-none text-l">Add Task</a>
+                </nav>
+            </div>
         </div>
-    @empty
-        <div>There is no tasks!</div>
-    @endforelse
+        <div class="row card-body ">
+            <div class="list-group list-group-numbered">
+                <ol class="d-flex flex-column gap-1 justify-content-center align-items-center">
+                    @forelse($tasks as $task)
+                        <li class="col-12 text-center">
+                            <div class="rounded-2">
+                                <a  href="{{ route('tasks.show', ['task' => $task->id]) }}"
+                                    @class(['list-group-item text-decoration-line-through text-success border border-success' => $task->completed,'list-group-item text-danger border border-danger' => !$task->completed ])> {{ $task->title }}</a>
+                            </div>
 
-    @if ($task->count())
-        <nav class="mt-4">
-            {{$tasks->links() }}
-        </nav>
-    @endif
+                        </li>
+                    @empty
+                        <div>There is no tasks!</div>
+                    @endforelse
+
+                    @if ($task->count())
+                        <nav class="my-5">
+                            {{$tasks->links() }}
+                        </nav>
+                    @endif
+                </ol>
+
+            </div>
+
+        </div>
+
+    </section>
+
 @endsection
 {{--        @if(count($tasks) > 0)--}}
 {{--            @foreach($tasks as $task)--}}
--- task-list-project/resources/views/layouts/app.blade.php
+++ task-list-project/resources/views/layouts/app.blade.php
@@ -6,11 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 10 Task List App</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    @yield('styles')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body class="container mx-auto mt-10 mb-10 max-w-lg">
-    <h1 class="mb-4 text-2xl">@yield('title')</h1>
+<body class="container mt-4 mb-5 d-flex flex-column ">
+    <h1 class="fs-1 text-center mb-4">@yield('title')</h1>
     <div>
         @if(session()->has('success'))
             <div>{{ session('success') }}</div>
@@ -18,5 +17,7 @@
 
         @yield('content')
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
